86d023535ed42c5ca86566a279c5ee66a23fd705
Sithu Khant <122719409+heysithu@users.noreply.github.com>
2026-04-19 17:56:54 +0630

    feat: hono rpc working on deployment

--- apps/web/src/libs/rpc.ts
+++ apps/web/src/libs/rpc.ts
@@ -2,7 +2,10 @@ import type { ApiType } from '@repo/api-type';
 import { hc } from 'hono/client';
 
 export const rpc = hc<ApiType>('https://sveltesse-turbo-api-dev.sithukhant.workers.dev');
+// export const rpc = hc<ApiType>('http://localhost:8787');
 
-export async function createRpc(fetch: typeof globalThis.fetch) {
-  return hc<ApiType>('https://sveltesse-turbo-api-dev.sithukhant.workers.dev', { fetch });
+export async function createRpc(env: Env) {
+  return hc<ApiType>('https://sveltesse-turbo-api-dev.sithukhant.workers.dev', {
+    fetch: env.API?.fetch.bind(env.API)
+  });
 }
--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,8 +1,9 @@
-import { rpc } from '~/libs/rpc';
+import { createRpc } from '~/libs/rpc';
 
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ platform }) => {
+  const rpc = await createRpc(platform?.env as Env);
   const res = await rpc.index.$get();
   const { message } = await res.json();
 
