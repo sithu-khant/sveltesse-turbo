c9026f9ff381f6336937687b52a695f6f0e7ee27
Sithu Khant <122719409+heysithu@users.noreply.github.com>
2026-04-21 10:26:45 +0630

    chore: add seo config to the web

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+  import { page } from '$app/state';
+</script>
+
+<div class='relative min-h-screen'>
+  <div class='absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center'>
+    <div class='mb-6'>
+      <h1 class='text-red-500 text-xl font-semibold'>Something went wrong</h1>
+      <p class='text-red-500 mt-2 max-w-lg text-sm'>
+        {page.error ? page.error.message : 'Something went wrong'}
+      </p>
+    </div>
+
+    <a href='/' class='underline'>Go to Home</a>
+  </div>
+</div>
